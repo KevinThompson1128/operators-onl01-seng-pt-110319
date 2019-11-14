8f04d926e075a858e79af3eaa6320ca79c2ba407
Kevin Thompson <indelible11@yahoo.com>
2019-11-14 05:35:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,25 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  else false
 end
-
-
-
-def not_safe?(speed)
-	
+  if speed < 40
+    return true
+end
+  if speed == 40 && 60
+    return false
+end
+end
+ 
+ def not_safe?(speed)
+	  if speed > 60
+    return true
+end
+  if speed < 40 ? true : false
+end
+  if speed < 40
+    return true
+end
 end
 	
 
